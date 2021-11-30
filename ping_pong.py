e46190cdb9b00b92c89e5dbe38a5a3967087d899
kirilll2288 <93859664+kirilll2288@users.noreply.github.com>
2021-11-30 16:55:10 +0200

    проект пинг понг
    добавил фон спрайты и игровой цикл

---
+++ ping_pong.py
@@ -0,0 +1,58 @@
+from pygame import *
+ 
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+display.set_caption("ping_pong")
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, width, height, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image),(width,height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+        self.width = width
+        self.height = height
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Wall(sprite.Sprite):
+    def  __init__(self, color_1, color_2, color_3, wall_width, wall_height, wall_x, wall_y):
+        super().__init__()
+        self.color_1 = color_1
+        self.color_2 = color_2
+        self.color_3 = color_3
+        self.width = wall_width
+        self.height = wall_height
+
+        self.image = Surface((self.width, self.height))
+        self.image.fill((color_1, color_2, color_3))
+        self.rect = self.image.get_rect()
+        self.rect.x = wall_x
+        self.rect.y = wall_y
+    def draw_wall(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+wall1 = Wall(166, 219, 8, 15, 220, 0, 150)
+wall2 = Wall(255, 0, 1, 15, 220, 685, 150)
+wall1.draw_wall()
+wall2.draw_wall()
+
+
+game = True
+FPS = 60
+clock = time.Clock()
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    wall1.draw_wall()
+    wall2.draw_wall()
+
+
+    display.update()
+    clock.tick(60)
